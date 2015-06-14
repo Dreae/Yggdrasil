7cbe7c31cad4275f9abacd2a920587d600bea2d1
Will Austin <penitenttangentt@gmail.com>
2015-06-13 21:34:54 -0400

    Switch docker args to cmd instead of entrypoint

--- daemon/docker.go
+++ daemon/docker.go
@@ -2,6 +2,8 @@ package daemon
 
 import (
   "fmt"
+  "log"
+  "strconv"
   "github.com/fsouza/go-dockerclient"
 )
 
@@ -34,9 +36,11 @@ func createServer(client *docker.Client, def ServerDefinition) error {
   for _, port := range def.Ports {
     pK := docker.Port(fmt.Sprintf("%d/%s", port.Container, port.Protocol))
     hostConfig.PortBindings[pK] = make([]docker.PortBinding, 1)
-    hostConfig.PortBindings[pK][0].HostPort = string(port.Host)
+    hostConfig.PortBindings[pK][0].HostIP = "0.0.0.0"
+    hostConfig.PortBindings[pK][0].HostPort = strconv.Itoa(port.Host)
   }
 
+  log.Println(hostConfig.PortBindings)
 
   createConfig.Config = &contConfig
   createConfig.HostConfig = &hostConfig
